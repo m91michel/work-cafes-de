307a7546bb50dcedad920883d337ed8b1e84d09a
Mathias Michel <m91michel@gmail.com>
2025-02-17 08:46:37 +0100

    fix sitemap generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -10,7 +10,8 @@ const siteUrl = process.env.SITE_URL || fallbackUrl;
 // Initialize Supabase client
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  { db: { schema: 'cafeforwork' } }
 );
 
 const excludeLocaleSpecificRoutes = isGerman
