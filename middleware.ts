3e87181b6cf31af88b748e374f93a028c08a2c61
Mathias Michel <m91michel@gmail.com>
2025-05-14 21:43:22 +0200

    disable middleware

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,22 @@
 import { NextRequest } from 'next/server'
 import { updateSession } from '@/libs/supabase/utils'
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request)
-}
+// export async function middleware(request: NextRequest) {
+//   return await updateSession(request)
+// }
+export default function middleware() { }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public (public files)
-     * - api (API routes)
-     */
-    '/((?!_next/static|_next/image|favicon.ico|public|api).*)',
-  ],
+  // matcher: [
+  //   /*
+  //    * Match all request paths except for the ones starting with:
+  //    * - _next/static (static files)
+  //    * - _next/image (image optimization files)
+  //    * - favicon.ico (favicon file)
+  //    * - public (public files)
+  //    * - api (API routes)
+  //    */
+  //   '/((?!_next/static|_next/image|favicon.ico|public|api).*)',
+  // ],
+  matcher: []
 } 
